b1c171bcbf7f126d0ab889665b0ba1426172580e
DragonXC2 <dragonaxc@gmail.com>
2017-12-07 06:39:29 -0800

    Express Package

--- First NodeJs Server/myapp/firstNode.js
+++ First NodeJs Server/myapp/firstNode.js
@@ -1,4 +1,6 @@
 var http = require('http');
+var express = require('express');
+var app = express();
 var port = 8080;
 var hostname = '127.0.0.1';
 var fs = require('fs');
@@ -7,24 +9,19 @@ var url = require('url');
 const server = http.createServer((req, res) => {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
-  if(filename!=''){
-    fs.readFile(filename, function(err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }
+  console.log(filename);
+  res.writeHead(301,{Location: 'http://127.0.0.1:8080/index.html'});
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+      console.log("404 Not Found");
+    }
+      console.log("something else");
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
       return res.end();
-    });
-  }
-  else{
-    fs.readFile('index.html',(err,data)=>{
-      res.writeHead(200,{'Content-Type':'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
+  });
 });
 
 server.listen(port,hostname,() => {
